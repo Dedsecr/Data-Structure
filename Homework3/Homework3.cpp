903e380f8dc5f0ce8f714774526ecfbdb1e34134
石翔宇 <xyu.shi@outlook.com>
2020-10-23 09:30:52 +0800

    update

--- Homework3/Homework3.cpp
+++ Homework3/Homework3.cpp
@@ -20,6 +20,29 @@ struct TreeNode
 };
 typedef TreeNode * Tree;
 typedef TreeNode * Node;
+void BuildTree(string &S, Tree &Root, int &P)
+{
+    if(P == S.length())
+        return;
+    if(S[P] != '#')
+    {
+        Root->data = S[P];
+        P++;
+        Root->LCh = new TreeNode;
+        BuildTree(S, Root->LCh, P);
+    }
+    else
+        P++;
+    if(S[P] != '#')
+    {
+        Root->data = S[P];
+        P++;
+        Root->RCh = new TreeNode;
+        BuildTree(S, Root->RCh, P);
+    }
+    else
+        P++;
+}
 void Get_PreOrder_WithRecursion(Tree &Root)
 {
     if(Root->LCh != NULL)
@@ -115,7 +138,7 @@ void Get_PostOrder_WithoutRecursion(Tree &Root)
     }
     puts("");
 }
-int Is_CompleteTree(Tree &Root, int Depth, bool &Is)
+int Is_CompleteTree_DFS(Tree &Root, int Depth, bool &Is)
 {
     if(!Root->LCh && Root->RCh)
     {
@@ -124,11 +147,11 @@ int Is_CompleteTree(Tree &Root, int Depth, bool &Is)
     }
     if(!Root->LCh && !Root->RCh)
         return Depth + 1;
-    int LDepth = Is_CompleteTree(Root->LCh, Depth + 1, Is);
+    int LDepth = Is_CompleteTree_DFS(Root->LCh, Depth + 1, Is);
     if(!Is)return LDepth;
     if(Root->RCh)
     {
-        int RDepth = Is_CompleteTree(Root->RCh, Depth + 1, Is);
+        int RDepth = Is_CompleteTree_DFS(Root->RCh, Depth + 1, Is);
         if(!Is)return max(LDepth, RDepth);
         if(LDepth < RDepth || LDepth - RDepth > 1)
             Is = 0;
@@ -141,4 +164,43 @@ int Is_CompleteTree(Tree &Root, int Depth, bool &Is)
         return LDepth;
     }
 }
-void Get_MaxWidth(Tree &Root, int * &Width)
+void Clear_Vis(Tree &Root)
+{
+    Root->vis = 0;
+    if(Root->LCh != NULL)
+        Clear_Vis(Root->LCh);
+    if(Root->RCh != NULL)
+        Clear_Vis(Root->RCh);
+}
+bool Is_CompleteTree(Tree &Root)
+{
+    bool Res = 1;
+    Clear_Vis(Root);
+    Is_CompleteTree_DFS(Root, 1, Res);
+    return Res;
+}
+void Get_MaxWidth_DFS(Tree &Root, int * Width, int Depth)
+{
+    Width[Depth]++;
+    if(Root->LCh != NULL)
+        Get_MaxWidth_DFS(Root->LCh, Width, Depth + 1);
+    if(Root->RCh != NULL)
+        Get_MaxWidth_DFS(Root->RCh, Width, Depth + 1);
+}
+int Get_MaxWidth(Tree &Root)
+{
+    int Width[MAXN] = {0}, Res = 0;
+    Get_MaxWidth_DFS(Root, Width, 1);
+    for (int i = 1; i < MAXN && Width[i]; ++i)
+        Res = max(Res, Width[i]);
+    return Res;
+}
+int main()
+{
+    string S;
+    int P = 0;
+    Tree Root = new TreeNode;
+    cin >> S;
+    BuildTree(S, Root, P);
+    return 0;
+}
