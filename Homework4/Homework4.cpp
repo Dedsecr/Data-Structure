b10570711ada53adb68fe3f6ee2e1d83667d4c5c
石翔宇 <xyu.shi@outlook.com>
2020-11-14 21:51:33 +0800

    pack

--- Homework4/Homework4.cpp
+++ Homework4/Homework4.cpp
@@ -10,7 +10,7 @@
 #include <cmath>
 using namespace std;
 const int MAXN = 50;
-
+//遍历结果的结构体
 struct Result
 {
     vector<int>Sequence;
@@ -23,6 +23,7 @@ struct Result
             TreeMatrix[i].clear();
         memset(ID, 0, sizeof(ID));
     }
+    //输出遍历结果
     void Print() 
     {
         puts("Sequence:");
@@ -38,12 +39,12 @@ struct Result
         puts("");
     }
 };
-
+//邻接矩阵的存储结构
 class AdjacencyMatrix
 {
 private:
-    int Matrix[MAXN][MAXN];
-    bool Visited[MAXN], Appeared[MAXN];
+    int Matrix[MAXN][MAXN];//邻接矩阵
+    bool Visited[MAXN], Appeared[MAXN];//该节点是否访问过；该节点是否出现过
     int n, m;//节点的数量和边的数量 
 
 public:
@@ -58,32 +59,43 @@ public:
         m = _m;
         
     }*/
+    //清空邻接矩阵
+    void Clear()
+    {
+        memset(Matrix, 0, sizeof(Matrix));
+        memset(Appeared, 0, sizeof(Appeared));
+    }
+    //遍历前初始化
     void Initialization()
     {
         for(int i = 0; i < MAXN; ++i)
             Visited[i] = 0;
     }
+    //获取x到y是否有边
     int GetMatrixEle(int x, int y)
     {
         return Matrix[x][y];
     }
+    //邻接矩阵插入
     void Insert(int x, int y)
     {
         Matrix[x][y] = 1;
         Appeared[x] = 1;
         Appeared[y] = 1;
     }
+    //建立邻接矩阵
     void InputandBuild()
     {
+        Clear();
         cin >> n >> m;
         for (int i = 1; i <= m; ++i)
         {
             int x, y;
             cin >> x >> y;
             Insert(x, y);
-            Insert(y, x);
         }
     }
+    //深度优先搜索的遍历过程
     void DFS(int x,int &Count, Result &Res)
     {
         Visited[x] = 1;
@@ -101,6 +113,7 @@ public:
             }
         }
     }
+    //用递归实现深度优先搜索
     Result DFSWithRecursion()
     {
         Result Res;
@@ -111,6 +124,7 @@ public:
                 DFS(i, Count, Res);
         return Res;
     }
+    //用非递归实现深度优先搜索
     Result DFSWithoutRecursion()
     {
         Result Res;
@@ -144,6 +158,7 @@ public:
             }
         return Res;
     }
+    //用非递归实现广度优先搜索
     Result BFSWithoutRecursion()
     {
         Result Res;
@@ -177,7 +192,18 @@ public:
             }
         return Res;
     }
+    void Print()
+    {
+        int Max = 0;
+        for (int i = 0; i < MAXN; ++i)
+            if(Appeared[i])
+                Max = max(Max, i);
+        for (int i = 0; i < Max; ++i, cout << '\n')
+            for (int j = 0; j < Max; ++j)
+                cout << Matrix[i][j] << " ";
+    }
 };
+//邻接表的存储结构
 class AdjacencyList
 {
 private:
@@ -190,8 +216,8 @@ private:
             Next = NULL;
         }
     };
-    Node *List[MAXN];
-    bool Visited[MAXN], Appeared[MAXN];
+    Node *List[MAXN];//点集
+    bool Visited[MAXN], Appeared[MAXN];//该节点是否访问过；该节点是否出现过
     int n, m;//节点的数量和边的数量
 
 public:
@@ -200,18 +226,19 @@ public:
         for (int i = 0; i < MAXN; ++i)
             List[i] = new Node, Appeared[i] = 0;
     }
-    /*AdjacencyList(int _n, int _m)
+    //清空邻接表
+    void Clear()
     {
-        n = _n;
-        m = _m;
         for (int i = 0; i < MAXN; ++i)
-            List[i] = NULL;
-    }*/
+            List[i] = new Node, Appeared[i] = 0;
+    }
+    //遍历前初始化
     void Initialization()
     {
         for(int i = 0; i < MAXN; ++i)
             Visited[i] = 0;
     }
+    //获取x的出度
     int GetListSize(int x)
     {
         int Res = 0;
@@ -223,6 +250,7 @@ public:
         }
         return Res;
     }
+    //获取x到y是否有边
     int GetListEle(int x, int y)
     {
         int Cnt = -1;
@@ -236,28 +264,30 @@ public:
         }
         return -1;
     }
+    //邻接表插入
     void Insert(int x, int y)
     {
-        Node* now = List[x];
-        while(now->Next != NULL)
-            now = now->Next;
-        now->Next = new Node;
-        now->Next->Next = NULL;
-        now->Next->To = y;
+        Node *now = List[x];
+        Node *New = new Node;
+        New->To = y;
+        New->Next = now->Next;
+        now->Next = New;
         Appeared[x] = 1;
         Appeared[y] = 1;
     }
+    //建立邻接表
     void InputandBuild()
     {
+        Clear();
         cin >> n >> m;
         for (int i = 1; i <= m; ++i)
         {
             int x, y;
             cin >> x >> y;
             Insert(x, y);
-            Insert(y, x);
         }
     }
+    //深度优先搜索的遍历过程
     void DFS(int x,int &Count, Result &Res)
     {
         Visited[x] = 1;
@@ -274,6 +304,7 @@ public:
             }
         }
     }
+    //用递归实现深度优先搜索
     Result DFSWithRecursion()
     {
         Result Res;
@@ -284,6 +315,7 @@ public:
                 DFS(i, Count, Res);
         return Res;
     }
+    //用非递归实现深度优先搜索
     Result DFSWithoutRecursion()
     {
         Result Res;
@@ -316,6 +348,7 @@ public:
             }
         return Res;
     }
+    //用非递归实现广度优先搜索
     Result BFSWithoutRecursion()
     {
         Result Res;
@@ -348,8 +381,22 @@ public:
             }
         return Res;
     }
+    void Print()
+    {
+        for (int i = 0; i < MAXN; ++i)
+        {
+            int Size = GetListSize(i);
+            if(Size)
+                cout << i << " : ";
+            else
+                continue;
+            for (int j = 0; j < Size; ++j)
+                cout << GetListEle(i, j) << " ";
+            cout << "\n";
+        }
+    }
 };
-
+//将邻接表转换为邻接矩阵
 AdjacencyMatrix Transformer_List2Matrix(AdjacencyList &AL)
 {
     AdjacencyMatrix Res;
@@ -363,6 +410,7 @@ AdjacencyMatrix Transformer_List2Matrix(AdjacencyList &AL)
     }
     return Res;
 }
+//将邻接矩阵转换为邻接表
 AdjacencyList Transformer_Matrix2List(AdjacencyMatrix &AM)
 {
     AdjacencyList Res;
@@ -375,56 +423,93 @@ AdjacencyList Transformer_Matrix2List(AdjacencyMatrix &AM)
 int main()
 {
     freopen("Homework4_In.txt","r",stdin);
-
-    printf("--------------------------------\n"
-           "0.Quit\n"
-           "1.Input Graph using Adjacency List\n"
-           "2.Input Graph using Adjacency Matrix\n"
-           "3.Transform List to Matrix\n"
-           "4.Transform Matrix to List\n"
-           "5.DFS List With Recursion\n"
-           "6.DFS Matrix With Recursion\n"
-           "7.DFS List Without Recursion\n"
-           "8.DFS Matrix Without Recursion\n"
-           "9.BFS List\n"
-           "10.BFS Matrix\n"
-           "--------------------------------\n"
-           "Input an Integer to Choose:\n");
-
+    //freopen("Homework4_Out.txt","w",stdout);
     AdjacencyMatrix AM;
-    AM.InputandBuild();
-    Result R;
-
-    puts("AdjacencyMatrix:");
-
-    puts("DFSWithRecursion");
-    R = AM.DFSWithRecursion();
-    R.Print();
-
-    puts("DFSWithoutRecursion");
-    R = AM.DFSWithoutRecursion();
-    R.Print();
-
-    puts("BFSWithoutRecursion");
-    R = AM.BFSWithoutRecursion();
-    R.Print();
-
-
-    puts("AdjacencyList:");
     AdjacencyList AL;
-    AL = Transformer_Matrix2List(AM);
-
-    puts("DFSWithRecursion");
-    R = AL.DFSWithRecursion();
-    R.Print();
-
-    puts("DFSWithoutRecursion");
-    R = AL.DFSWithoutRecursion();
-    R.Print();
-
-    puts("BFSWithoutRecursion");
-    R = AL.BFSWithoutRecursion();
-    R.Print();
+    Result R;
+    while(1)
+    {
+        int OP;
+        printf("--------------------------------\n"
+                "0.Quit\n"
+                "1.Input Graph using Adjacency List\n"
+                "2.Input Graph using Adjacency Matrix\n"
+                "3.Transform List to Matrix\n"
+                "4.Transform Matrix to List\n"
+                "5.DFS List With Recursion\n"
+                "6.DFS Matrix With Recursion\n"
+                "7.DFS List Without Recursion\n"
+                "8.DFS Matrix Without Recursion\n"
+                "9.BFS List\n"
+                "10.BFS Matrix\n"
+                "--------------------------------\n"
+                "Input an Integer to Choose:\n");
+        cin >> OP;
+        if(OP < 0 || OP > 10)
+        {
+            puts("Input Error!");
+            continue;
+        }
+        if(OP == 0)
+            break;
+        else if(OP == 1)
+        {
+            AL.InputandBuild();
+            AL.Print();
+        }
+        else if(OP == 2)
+        {
+            AM.InputandBuild();
+            AM.Print();
+        }
+        else if(OP == 3)
+        {
+            AM = Transformer_List2Matrix(AL);
+            AM.Print();
+        }
+        else if(OP == 4)
+        {
+            AL = Transformer_Matrix2List(AM);
+            AL.Print();
+        }
+        else if(OP == 5)
+        {
+            puts("DFSWithRecursion");
+            R = AL.DFSWithRecursion();
+            R.Print();
+        }
+        else if(OP == 6)
+        {
+            puts("DFSWithRecursion");
+            R = AM.DFSWithRecursion();
+            R.Print();
+        }
+        else if(OP == 7)
+        {
+            puts("DFSWithoutRecursion");
+            R = AL.DFSWithoutRecursion();
+            R.Print();
+        }
+        else if(OP == 8)
+        {
+            puts("DFSWithoutRecursion");
+            R = AM.DFSWithoutRecursion();
+            R.Print();
+        }
+        else if(OP == 9)
+        {
+            puts("BFSWithoutRecursion");
+            R = AL.BFSWithoutRecursion();
+            R.Print();
+        }
+        else if(OP == 10)
+        {
+            puts("BFSWithoutRecursion");
+            R = AM.BFSWithoutRecursion();
+            R.Print();
+        }
+    }
+    
     return 0;
 }
 /*
